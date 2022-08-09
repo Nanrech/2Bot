6f3f29b104bb84f61832876db232f56a244b2604
Nan R <nanbusiness13@gmail.com>
2022-08-09 19:09:23 +0200

    Initial commit 2 electric boogaloo

---
+++ index.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.js')
+const prefix = '.'
+const client = new Discord.Client({
+    allowedMentions:{
+        parse:["users"],
+        repliedUser: false
+    },
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_PRESENCES",
+        "GUILD_MEMBERS",
+        "GUILD_MESSAGE_REACTIONS"
+    ],
+});
+
+client.on("ready", () => {
+    console.log(`I'm ready! Logged in as ${client.user.tag}!`);
+    client.user.setActivity(
+        "Hellcastle & Tylerrrr",
+        {type: "WATCHING"}
+        );
+})
+
+client.on('message', async message => {
+    if (message.author.id == "399049916757966848" || message.author.id == "313350346724343809" || message.author.id == "291620843363106828") {
+        if (message.mentions?.users?.first() != null) {
+            if (message.content.includes("-")) {
+                let msg = message.content.split("-")?.[1]?.split(" ")
+                if (!isNaN(msg?.[0]) && parseInt(msg?.[0]) >= 1 && msg?.[0] != "") {
+                    if (msg?.[1].toLowerCase() == "social" && msg?.[2].toLowerCase().startsWith("credit")) {
+                        message.reply(new Discord.MessageEmbed()
+                            .setColor("#FF0000")
+                            .setDescription(`:rage: **${parseInt(msg?.[0])} Social Credit** has been deducted from <@${message.mentions?.users?.first().id}>!`)
+                            .setTitle('Great Shame!'))
+                    }
+                }
+            }
+            if (message.content.includes("+")) {
+                let msg = message.content.split("+")?.[1]?.split(" ")
+                if (!isNaN(msg?.[0]) && parseInt(msg?.[0]) >= 1 && msg?.[0] != "") {
+                    if (msg?.[1].toLowerCase() == "social" && msg?.[2].toLowerCase().startsWith("credit")) {
+                        message.reply(new Discord.MessageEmbed()
+                            .setColor("#00FF00")
+                            .setDescription(`<:uwuangel:750761120377339955> **${parseInt(msg?.[0])} Social Credit** has been granted to <@${message.mentions?.users?.first().id}>!`)
+                            .setTitle('Great Honor!'))
+                    }
+                }
+            }
+        }
+    }
+}
+);
+
+client.on("messageCreate", async message => {
+    if (message.author.id == "399049916757966848" || message.author.id == "313350346724343809" || message.author.id == "291620843363106828" || message.author.id == "452954731162238987"){
+        let msg = message.content.split("-")?.[1]?.split(" ")
+        if (msg[1] != "social" || msg[2] != "credit" || msg[3]){
+            return
+        }
+        if (message.mentions?.users?.first() != null){
+            if (message.content.startsWith('-')){
+                if (Number(msg[0] != NaN)){
+                }
+            }
+        }
+    }
+
+});
+
+client.login("OTI0NzE2OTI4NDgyODMyNDI2.G9MjFf._GdSe-mNxoAwy9qtqzYAoleET9rayPDFJwYvFQ")
---
+++ thingtesting.js
@@ -0,0 +1,3 @@
+let msg = "-100 social credit <@9287463982374>".split("-")?.[1]?.split(" ");
+let num = Number(msg[0]);
+console.log(msg);
