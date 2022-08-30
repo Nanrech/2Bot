173e9bb7f3917a06493c0a998550fff811f748f4
Nan R <nanbusiness13@gmail.com>
2022-08-30 19:25:32 +0200

    Create member-schema.js

---
+++ src/schemas/member-schema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema({
+	id: { type: String },
+	xp: { type: Number, default: 0 },
+	level: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('Members', memberSchema);
