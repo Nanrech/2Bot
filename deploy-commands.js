9e45d65229bd92821c78d7a7b2c60d1a815303be
Nan R <nanbusiness13@gmail.com>
2022-08-30 16:02:41 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@ const { Routes } = require('discord.js');
 const { clientId, token } = require('./src/config.json');
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, 'src/commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
