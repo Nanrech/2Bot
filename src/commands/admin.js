9b875c531009d6990f604bae8a79417e3250c440
Nan R <nanbusiness13@gmail.com>
2022-08-30 19:25:36 +0200

    Update admin.js

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { addXP } = require('../events/messageCreate');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -28,12 +29,42 @@ module.exports = {
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('mult')
-				.setDescription('Sets the random XP multiplier. 0 is NO random XP bonus and 100 means 2x XP'))
+				.setDescription('Sets the random XP multiplier. 0 is NO random XP bonus and 100 means 2x XP')
+				.addIntegerOption(option =>
+					option
+						.setName('mult-value')
+						.setDescription('XP multiplier value')
+						.setRequired(true)))
 		.addSubcommand(subcommand =>
 			subcommand
-				.setName('basexp')
-				.setDescription('Sets the base XP value')),
+				.setName('base-xp')
+				.setDescription('Sets the base XP value')
+				.addIntegerOption(option =>
+					option
+						.setName('bexp-value')
+						.setDescription('Base XP value')
+						.setRequired(true)))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('reaction-roles')
+				.setDescription('Sends an embed with all the reaction roles available. DO NOT USE IF YOU DON\'T KNOW WHAT YOU\'RE DOING'))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('mass-xp')
+				.setDescription('Runs through ALL members & assigns XP based on their roles. Handle with care!')),
 	async execute(interaction) {
-		interaction.reply(`Command ran: \`${interaction.options.getSubcommand()}\` (this does literally nothing atm, mald, cope, seethe)`);
+		if (interaction.options.getSubcommand() != 'mass-xp') {interaction.reply(`Command ran: \`${interaction.options.getSubcommand()}\` (this does literally nothing atm)`);}
+		if (interaction.user.id != '452954731162238987') {interaction.reply('No, shut up');}
+		// console.log(interaction.client.guilds.cache.get('707341019275853847').members.cache);
+		// interaction.guild.roles.fetch('415665311828803584').members.map(m => m.user.tag);
+		// interaction.guild.members.fetch();
+		let allMembers;
+		interaction.guild.members.fetch()
+			.then(value => {
+				allMembers = value;
+			})
+			.catch(error => console.error(error));
+		const t = allMembers.filter(member => member.roles.cache.has('780428613962956810'));
+		addXP;
 	},
 };
