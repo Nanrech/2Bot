bdf4948f04ef9215f1d680cd9a70378070e7b89d
Nan R <nanbusiness13@gmail.com>
2022-10-11 23:59:48 +0200

    sorry lol x2

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -7,9 +7,12 @@ module.exports = {
 	once: false,
 	execute(member) {
 		console.log('someone left');
+		// I don't know if this works, probably not.
+		// It's supposed to remove the person who left
+		// from the DB and send a message in logs about it with their XP & level
 		addXP(member.id, 0).then(final => {
-			member.client.channels.cache.get('832303064513380373').send(`${member} left. XP & level at the time of death: ${final.xp} -> ${getLevel(final.xp)}`);
-			memberSchema.findOneAndRemove(member.id);
+			member.client.channels.cache.get('832303064513380373').send(`${member} left. XP & level at the time of death: ${final.xp}xp, lvl ${getLevel(final.xp)}`);
+			memberSchema.findOneAndRemove(String(member.id));
 		});
 	},
 };
