1ec759c261860e443dd5e33faa1eefa4a196f0e7
Nan R <nanbusiness13@gmail.com>
2023-02-10 18:59:51 +0100

    shut up eslint

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,7 @@ const mongoose = require('../mongo');
 module.exports = {
 	name: 'ready',
 	once: true,
+	// eslint-disable-next-line no-unused-vars
 	execute(client) {
 		console.log('Connected to Discord');
 		mongoose();
