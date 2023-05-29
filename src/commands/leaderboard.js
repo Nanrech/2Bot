99d393c0fa24ec223f53b224e642298b16d4177d
Nan R <nanbusiness13@gmail.com>
2023-05-30 01:21:21 +0200

    Split /get into two separate commands

---
+++ src/commands/leaderboard.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const memberModel = require('../schemas/member');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('leaderboard')
+		.setDescription('Displays the top 15 biggest nerds'),
+	async execute(interaction) {
+		// OLD: Leaderboard shenanigans
+		// NEW: Here lie my hopes and dreams of making a better looking leaderboard embed. I was bested by JS' unrelenting bullshit.
+		const topMembers = await memberModel
+			.find()
+			.sort({ xp: -1 })
+			.limit(15);
+		if (!topMembers) return interaction.reply('Couldn\'t get rank. Do you have any XP?');
+
+		let usernames = '';
+		let levels = '';
+		let xp = '';
+
+		for (let i = 0; i != 15; i++) {
+			// https://stackoverflow.com/questions/60607341/displaying-leaderboard-in-embed
+			usernames += `\`${(i + 1).toString().padStart(2, '0')}\` <@${topMembers[i].id}>\n`;
+			levels += `\`${getLevel(topMembers[i].xp)}\`\n`;
+			xp += `\`${topMembers[i].xp.toLocaleString('en-us')}\`\n`;
+		}
+
+		usernames = usernames
+			.replace('`01`', '`🏆`')
+			.replace('`02`', '`🥈`')
+			.replace('`03`', '`🥉`');
+
+		const leaderboardEmbed = new EmbedBuilder()
+			.setColor(0xED4245)
+			.addFields(
+				{ name: 'Top 15 members', value: usernames, inline: true },
+				{ name: 'Level', value: levels, inline: true },
+				{ name: 'XP', value: xp, inline: true },
+			);
+
+		interaction.reply({ embeds: [leaderboardEmbed] });
+	},
+};
+
+
+function getLevel(xp) {
+	const XP = Number(xp);
+	if (XP < 300) {return 0;}
+	return Math.floor((-1 + Math.sqrt((XP / 37.5) + 1)) / 2) + 1;
+}
---
+++ src/commands/rank.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const memberModel = require('../schemas/member');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('rank')
+		.setDescription('Displays XP & level')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('Whose XP you want to get. Defaults to yourself.')
+				.setRequired(false)),
+	async execute(interaction) {
+		const member = interaction.options.getUser('target') == null ? interaction.user : interaction.options.getUser('target');
+		const memberDocument = await memberModel.findOne({ id: member.id }).exec();
+
+		if (!memberDocument) return interaction.reply('Something went wrong fetching that document from the database. Wait a moment before using again');
+		// If it's null we're fucked I guess
+
+
+		const rankEmbed = new EmbedBuilder()
+			.setColor(0xED4245)
+			.setTitle(`@${member.username}`)
+			.addFields([
+				{
+					'name': 'Level',
+					'value': `\`${getLevel(memberDocument.xp)}\``,
+					'inline': true,
+				},
+				{
+					'name': 'XP',
+					'value': `\`${memberDocument.xp.toLocaleString('en-us')}\``,
+					'inline': true,
+				},
+				{
+					'name': 'Next Level',
+					'value': `\`${(getReqXP(getLevel(memberDocument.xp) + 1) - memberDocument.xp).toLocaleString('en-us')}\``,
+					'inline': true,
+				},
+			])
+			.setThumbnail(member.displayAvatarURL())
+			.setFooter({ text: memberDocument._id.toString() });
+
+		interaction.reply({ embeds: [rankEmbed] });
+	},
+};
+
+function getReqXP(l) {
+	return (150 * (Math.pow(l, 2) + l));
+}
+
+function getLevel(xp) {
+	const XP = Number(xp);
+	if (XP < 300) {return 0;}
+	return Math.floor((-1 + Math.sqrt((XP / 37.5) + 1)) / 2) + 1;
+}
