43f8f3412f5bbf3ebfda39d90760fbcd18319f38
Nan <88463477+Nanrech@users.noreply.github.com>
2026-01-19 14:15:59 +0100

    Fix strictQuery DeprecationWarning when connecting to MongoDB

--- src/mongo.js
+++ src/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { mongoURL } = require('./config.json');
 
 module.exports = async () => {
-	await mongoose.connect(mongoURL, { keepAlive: true });
 	mongoose.set('strictQuery', true);
+	await mongoose.connect(mongoURL, { keepAlive: true });
 	return mongoose.connection.readyState;
 };
